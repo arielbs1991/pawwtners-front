b2a64cbea3473265c9d4f90c8e8b92d535e1472d
Mike Shenk <mlshenk415@gmail.com>
2021-01-07 16:15:18 -0800

    initial pages setup

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Layout} from 'antd';
+
+const { Footer } = Layout;
+
+export default function FooterComp() {
+    return (
+      
+    <Footer style={{ textAlign: 'center', height: 'calc(100vh - 55)' }}>Pawwtners ©2021</Footer>
+ 
+    )
+}
